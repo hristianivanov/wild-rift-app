e85ee31ab30acd8a8218161328004be7231d3272
Hristian Ivanow <hristianfancha@gmail.com>
2026-05-27 01:48:30 +0300

    Add champions API endpoint and data provider
    Introduce ChampionsController with a GET /api/champions action that returns champion data via dependency-injected ChampionDataProvider. Add ChampionDataProvider which supplies a hard-coded list of Champion models (Malphite, Garen, Fiora, Camille) with roles and tags for testing. Also update the .http collection to include a GET request for /api/champions and add separators for requests.

---
+++ WildRiftCounterLab.Api/WildRiftCounterLab.Api/Controllers/ChampionsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WildRiftCounterLab.Api.Data;
+
+namespace WildRiftCounterLab.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ChampionsController : ControllerBase
+{
+    private readonly ChampionDataProvider _championDataProvider;
+
+    public ChampionsController(ChampionDataProvider championDataProvider)
+    {
+        _championDataProvider = championDataProvider;
+    }
+
+    [HttpGet]
+    public IActionResult GetChampions()
+    {
+        var champions = _championDataProvider.GetChampions();
+
+        return Ok(champions);
+    }
+}
---
+++ WildRiftCounterLab.Api/WildRiftCounterLab.Api/Data/ChampionDataProvider.cs
@@ -0,0 +1,37 @@
+﻿using WildRiftCounterLab.Api.Models;
+
+namespace WildRiftCounterLab.Api.Data;
+
+public class ChampionDataProvider
+{
+    public List<Champion> GetChampions()
+    {
+        return new List<Champion>
+        {
+            new Champion
+            {
+                Name = "Malphite",
+                Roles = new List<string> { "Baron", "Support" },
+                Tags = new List<string> { "tank", "engage", "anti-ad" }
+            },
+            new Champion
+            {
+                Name = "Garen",
+                Roles = new List<string> { "Baron" },
+                Tags = new List<string> { "fighter", "safe", "sustain" }
+            },
+            new Champion
+            {
+                Name = "Fiora",
+                Roles = new List<string> { "Baron" },
+                Tags = new List<string> { "fighter", "duelist", "true-damage" }
+            },
+            new Champion
+            {
+                Name = "Camille",
+                Roles = new List<string> { "Baron" },
+                Tags = new List<string> { "fighter", "mobile", "true-damage" }
+            }
+        };
+    }
+}
